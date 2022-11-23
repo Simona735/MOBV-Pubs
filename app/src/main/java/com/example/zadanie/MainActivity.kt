ab31b421eaca86a82def289be3f4fe47db21f35d
Simona Richterová <richterova735@gmail.com>
2022-11-23 17:56:07 +0100

    remove redundant code

--- app/src/main/java/com/example/zadanie/MainActivity.kt
+++ app/src/main/java/com/example/zadanie/MainActivity.kt
@@ -21,14 +21,8 @@ class MainActivity : AppCompatActivity() {
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
 
+        //https://stackoverflow.com/questions/57187628/navigation-components-onsupportnavigateup
         val appBarConfiguration = AppBarConfiguration(setOf(R.id.login_fragment, R.id.sign_up_fragment, R.id.bars_fragment))
         setupActionBarWithNavController(navController, appBarConfiguration)
-
-        findViewById<BottomNavigationView>(R.id.nav_view)
-            .setupWithNavController(navController)
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        return Navigation.findNavController(this, R.id.nav_host_fragment).navigateUp()
     }
 }
