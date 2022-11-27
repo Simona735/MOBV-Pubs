17ad7b5740dc49c4e751760c4506e45288a415fb
Simona Richterová <richterova735@gmail.com>
2022-11-27 21:11:22 +0100

    add pubs icon

--- app/src/main/java/com/example/zadanie/ui/widget/barlist/BarsAdapter.kt
+++ app/src/main/java/com/example/zadanie/ui/widget/barlist/BarsAdapter.kt
@@ -4,10 +4,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.content.ContextCompat.getDrawable
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.zadanie.R
 import com.example.zadanie.data.db.model.BarItem
+import com.example.zadanie.helpers.PreferenceData
 import com.example.zadanie.helpers.autoNotify
+import com.example.zadanie.ui.fragments.BarsFragmentDirections
 import com.google.android.material.chip.Chip
 import kotlin.properties.Delegates
 
@@ -40,7 +45,38 @@ class BarsAdapter(val events: BarsEvents? = null) :
         fun bind(item: BarItem, events: BarsEvents?) {
             itemView.findViewById<TextView>(R.id.name).text = item.name
             itemView.findViewById<TextView>(R.id.count).text = item.users.toString()
-            itemView.findViewById<Chip>(R.id.type).text = item.type.replace('_', ' ')
+            itemView.findViewById<Chip>(R.id.type).text = item.type
+                    .replace("node", "place")
+                    .replace('_', ' ')
+
+            val drawable : Int = when (item.type){
+                "cafe" -> {
+                    R.drawable.ic_baseline_local_cafe_24
+                }
+                "restaurant" -> {
+                    R.drawable.ic_baseline_restaurant_24
+                }
+                "pub" -> {
+                    R.drawable.ic_baseline_sports_bar_24
+                }
+                "fast_food" -> {
+                    R.drawable.ic_baseline_fastfood_24
+                }
+                "bar" -> {
+                    R.drawable.ic_baseline_local_bar_24
+                }
+                "nightclub" -> {
+                    R.drawable.ic_baseline_nightlife_24
+                }
+                "stripclub" -> {
+                    R.drawable.ic_baseline_wine_bar_24
+                }
+                else -> {
+                    R.drawable.ic_baseline_local_drink_24
+                }
+            }
+
+            itemView.findViewById<Chip>(R.id.type).chipIcon = getDrawable(parent.context, drawable)
 
             itemView.setOnClickListener { events?.onBarClick(item) }
         }
